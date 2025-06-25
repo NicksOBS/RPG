6fbb77c036b8c9674148f4baa1700e1c9894236c
matheus <no9946191@gmail.com>
2025-06-24 22:44:43 -0300

    main

--- Classes/main.cpp
+++ Classes/main.cpp
@@ -11,26 +11,50 @@
 
 
 using namespace std;
-int main(){
-    // cout << "Insira seu nome: ";
-    // string nome;
-    // cin >> nome;
-    Entidade *m = new Mago();
-    Entidade *g = new Guerreiro();
-    Jogador p1{"Jogador 1", m};
-    Jogador p2{"Jogador 2", g};
-    // cout << "\nNome: " << p1.get_jogador_nome() << endl << "Classe: " << p1.get_jogador_classe_nome() << endl;
-    // Graveto_do_aprendiz item1;
-    // Espada_enferrujada item2;
+int main() {
+    string nome;
+    int escolha_classe;
 
-    // if(p1.mochila_vazia()) cout << "MOCHILA VAZIA\n"; 
-    // p1.mochila_inserir(item1);
-    // p1.mochila_inserir(item2);
-    // p1.mochila_mostrar();
-    // p1.mochila_remover(item1);
-    // p1.mochila_mostrar();
-    // cout << "DANO: " << p1.get_jogador_classe()->ataque();//da pra atacar usando a funcao get_jogador_classe
-    p1.set_jogador_dinheiro(100);
-    eventoAleatorio(p1);
-    mostrar_status_batalha(p1, *p2.get_jogador_classe());
+    cout << "Digite seu nome, Fragmentado: ";
+    cin >> nome;
+
+    cout << "Bem-vindo(a) a Leyndell, " << nome << "!" << endl;
+    cout << "Um lamento ecoa pelos Confins Interiores de Eldoria, uma terra há muito esquecida, dilacerada por calamidades e ecos de uma glória desvanecida." << endl;
+    cout << "Outrora, grandes reinos ergueram-se sob a benção do Anel Primordial, mas sua fratura trouxe a Ruína, espalhando uma mancha que corrompe almas e terras." << endl;
+    cout << "Você, um Fragmentado, despertou em meio às cinzas e sussurros de um tempo esquecido, com a memória fragmentada e um propósito incerto." << endl;
+    cout << "O que resta da antiga chama da ambição bruxuleia em seu peito, impulsionando-o(a) a buscar o que foi perdido, ou a forjar um novo destino na escuridão!!" << endl << endl;
+
+    Entidade* jogador = nullptr;
+
+    try {
+        cout << "A névoa do esquecimento se dissipa, revelando os caminhos que você pode trilhar. Qual a sua verdadeira natureza?" << endl;
+        cout << "1. Mago: Aqueles que buscam o conhecimento proibido nas sombras, manipulando as energias arcanas esquecidas." << endl;
+        cout << "2. Guerreiro: Um remanescente de ordens caídas, empunhando lâminas enferrujadas e a força bruta de um passado glorioso." << endl;
+        cout << "Digite o número da sua escolha, Fragmentado: ";
+
+        switch (escolha_classe) {
+            case 1: {
+                jogador = new Mago();
+                cout << "Você escolheu o caminho do Mago!" << endl;
+                cout << "Que as névoas do arcano o(a) guiem, " << nome << "." << endl;
+                break;
+            }
+            case 2: {
+                jogador = new Guerreiro();
+                cout << "Você escolheu ser um guerreiro!" << endl;
+                cout << "Que a sua lâmina nunca falhe, " << nome << "." << endl;
+                break;
+            }
+            default:
+                throw invalid_argument("Escolha inválida. Por favor, escolha 1 ou 2.");
+        }
+    } catch (const exception& e) {
+        cerr << "Erro: " << e.what() << endl;
+    }
+
+    
+
+
+    return 0;
 }
+
